38eb856662cff4a126e3a753f4cf84d0c0740614
Xenos Konstantinos <mr.xkon@gmail.com>
2018-09-18 20:08:59 +0300

    refine policy text
    refine policy text

--- policy-text.php
+++ policy-text.php
@@ -1,10 +1,10 @@
 <div class="wp-suggested-text">
 	<h2><?php esc_html_e( 'Additional information', 'user-sync' ); ?></h2>
 	<p class="privacy-policy-tutorial">
-		<?php _e( 'User Synchronization links user profiles across multiple single WordPress installations. It does not retain any additional data on its own but copies and synchronizes the existing user information between multiple installations. We recommend informing your users with which websites you are syncing their data with and what that set of data contains.', 'user-sync' ); ?>
+		<?php _e( 'User Synchronization links user profiles across multiple single WordPress installations. It does not retain any additional data on its own but copies and synchronizes the existing user information between multiple installations. We recommend informing users their information is being shared, what websites their data is stored on and what information is being synced.', 'user-sync' ); ?>
 	</p>
 	<p>
 		<strong class="privacy-policy-tutorial"><?php esc_html_e( 'Suggested text: ', 'user-sync' ); ?></strong>
-		<?php _e( 'When you register or update your user information with our website, your user data is synced accross these sites: example1.com, example2.com, example3.com. The information synced includes: Username, First Name, Last Name, Email...', 'user-sync' );  ?>
+		<?php _e( 'When you register or update your user information with our website, your user data is synced across these sites: example1.com, example2.com, example3.com. The information synced includes: Username, First Name, Last Name, Email, etc.', 'user-sync' );  ?>
 	</p>
 </div>
